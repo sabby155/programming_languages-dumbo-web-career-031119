e4e0ff7b3d26925750f494c7493273e8b93ad1ba
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-26 15:38:26 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.collect do |lang_style, lang_data|
     lang_data.collect do |lang, lang_types|
-     new_hash[lang] ||= lang_types
+     new_hash[lang] = lang_types
      new_hash[lang][:style]=[]
      new_hash[lang][:style]<< lang_style
     end
