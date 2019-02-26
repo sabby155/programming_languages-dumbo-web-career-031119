617d88ae9287eef081ec895ec40395defbcd0ad9
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-26 15:53:14 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.collect do |lang_style, lang_data|
+    lang_data.collect do |lang, lang_types|
+     new_hash[lang] = lang_types
+     new_hash[lang][:style]=[]
+     new_hash[lang][:style]<< lang_style
+    end
+  end
+  new_hash[:javascript][:style] << :oo #since JS is both func & oo - it goes into the same style array 
+  new_hash
 end
+
+
+
+
+
