1c1e84b463d6bfeff00dcb200bab46b1e7f3e97e
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-26 15:25:42 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,7 +7,7 @@ def reformat_languages(languages)
      new_hash[lang][:style]<< lang_style
     end
   end
-  new_hash[:javascript][:style] << :oo #since JS is both func & oo
+  new_hash[:javascript][:style] << :oo #since JS is both func & oo - it goes into the same style array 
   new_hash
 end
 
